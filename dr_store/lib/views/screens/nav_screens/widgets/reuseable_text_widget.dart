ab7b38c12f5380cb209677dd46ea00e7cffbdbe6
KDHBuddhika <136336705+KDHBuddhika@users.noreply.github.com>
2025-05-20 23:46:23 +0530

    edit profile screen

---
+++ dr_store/lib/views/screens/nav_screens/widgets/reuseable_text_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ReuseableTextWidget extends StatelessWidget {
+  final String title;
+  final String subtitle;
+
+  const ReuseableTextWidget({super.key, required this.title , required this.subtitle});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            title,
+            style: GoogleFonts.roboto(
+                fontSize: 14, fontWeight: FontWeight.bold, letterSpacing: 1),
+          ),
+          Text(subtitle,
+              style: GoogleFonts.roboto(
+                  color: Colors.red.shade600,
+                  fontSize: 13,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1)),
+        ],
+      ),
+    );
+  }
+}
